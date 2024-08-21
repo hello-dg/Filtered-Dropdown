c9a99f5f5856d1892aaf0639429782e734754fc5
David <hellodg.py@gmail.com>
2024-08-21 13:37:46 -0700

    Add scroll to dropdown

--- server.py
+++ server.py
@@ -2,7 +2,32 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-clients = ["Marvel Entertainment", "Reynolds, Ryan & Blake", "Wilson, Wade", "Stark, Tony", "Wayne, Bruce", "DC Entertainment", "Warner Bros.", "Wilson, Owen"]
+clients = ["Marvel Entertainment",
+           "Reynolds, Ryan & Blake", 
+           "Wilson, Wade", 
+           "Stark, Tony", 
+           "Wayne, Bruce", 
+           "DC Entertainment", 
+           "Warner Bros.", 
+           "Wilson, Owen",
+           "Murdock, Matt",
+           "Jones, Jessica",
+           "Lucas, Carl",
+           "Thomas, Daniel",
+           "Savage, Clark",
+           "Strange, Stephen",
+           "Wilson, Samuel",
+           "Richards, Reed",
+           "Storm, Sue",
+           "Grimm, Benjamin",
+           "Blaze, Johnathon",
+           "Queen, Oliver",
+           "Quill, Peter",
+           "Barton, Clinton",
+           "Banner, Bruce"
+]
+
+
 @app.route('/')
 def index():
     return render_template('filter-drop.html', clients=clients)
